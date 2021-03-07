73b72f99013569ec31428aa84e054489b57a3dfe
fthomasvp <fellipecab@gmail.com>
2021-03-07 20:35:26 -0300

    test(dynamic-list-display): add unit tests

--- src/modules/components/dynamic-list-display/index.tsx
+++ src/modules/components/dynamic-list-display/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { formatDistanceToNow } from 'date-fns';
 
 import ClayCard from '@clayui/card';
 import ClayLayout from '@clayui/layout';
@@ -16,98 +17,135 @@ const DynamicDisplay = ({ repositories }: Props): JSX.Element => {
   return (
     <ClayLayout.ContainerFluid view>
       <ClayLayout.Row>
-        {repositories.map((repo) => (
-          <div key={repo.id} className="col-md-4">
-            <ClayCard>
-              <ClayCard.Row style={{ height: '50px', alignItems: 'center' }}>
-                <img
-                  src={lifeRayLogo}
-                  alt="Liferay logo"
-                  style={{ marginLeft: '6px' }}
-                />
+        {repositories.map(
+          ({
+            id,
+            full_name,
+            stargazers_count,
+            forks_count,
+            open_issues_count,
+            created_at,
+            lastCommitAt,
+            license,
+            language,
+          }) => (
+            <div key={id} className="col-md-4">
+              <ClayCard>
+                <ClayCard.Row style={{ height: '50px', alignItems: 'center' }}>
+                  <img
+                    src={lifeRayLogo}
+                    alt="Liferay logo"
+                    style={{ marginLeft: '6px' }}
+                  />
 
-                <ClayLayout.Col size={8}>
-                  <ClayCard.Description displayType="title">
-                    {repo.full_name}
-                  </ClayCard.Description>
-                </ClayLayout.Col>
+                  <ClayLayout.Col size={8}>
+                    <ClayCard.Description
+                      data-testid="fullName"
+                      displayType="title"
+                    >
+                      {full_name}
+                    </ClayCard.Description>
+                  </ClayLayout.Col>
 
-                <ClayButtonWithIcon
-                  aria-label="Star"
-                  displayType="unstyled"
-                  symbol="star-o"
-                />
+                  <ClayButtonWithIcon
+                    aria-label="Star"
+                    displayType="unstyled"
+                    symbol="star-o"
+                  />
 
-                <ClayButtonWithIcon
-                  aria-label="Trash"
-                  displayType="unstyled"
-                  symbol="trash"
-                />
-              </ClayCard.Row>
-
-              <div
-                className="card-divider"
-                style={{ marginTop: '0px', marginBottom: '0px' }}
-              />
-
-              <ClayCard.Body>
-                <ClayCard.Row>
-                  <div className="autofit-col autofit-col-expand">
-                    <section className="autofit-section">
-                      <ClayCard.Description displayType="title">
-                        Stars{' '}
-                        <span style={{ fontWeight: 400 }}>
-                          {repo.stargazers_count}
-                        </span>
-                      </ClayCard.Description>
-
-                      <ClayCard.Description displayType="title">
-                        Forks{' '}
-                        <span style={{ fontWeight: 400 }}>
-                          {repo.forks_count}
-                        </span>
-                      </ClayCard.Description>
-
-                      <ClayCard.Description displayType="title">
-                        Open issues{' '}
-                        <span style={{ fontWeight: 400 }}>
-                          {repo.open_issues_count}
-                        </span>
-                      </ClayCard.Description>
-
-                      <ClayCard.Description displayType="title">
-                        Age{' '}
-                        <span style={{ fontWeight: 400 }}>
-                          {repo.created_at}
-                        </span>
-                      </ClayCard.Description>
-
-                      <ClayCard.Description displayType="title">
-                        Last commit{' '}
-                        <span style={{ fontWeight: 400 }}>
-                          {repo.lastCommitAt || 'No commits yet'}
-                        </span>
-                      </ClayCard.Description>
-
-                      <ClayCard.Description displayType="title">
-                        License{' '}
-                        <span style={{ fontWeight: 400 }}>
-                          {repo.license?.name || 'N/A'}
-                        </span>
-                      </ClayCard.Description>
-
-                      <ClayCard.Caption>
-                        <ClayLabel displayType="danger">
-                          {repo.language}
-                        </ClayLabel>
-                      </ClayCard.Caption>
-                    </section>
-                  </div>
+                  <ClayButtonWithIcon
+                    aria-label="Trash"
+                    displayType="unstyled"
+                    symbol="trash"
+                  />
                 </ClayCard.Row>
-              </ClayCard.Body>
-            </ClayCard>
-          </div>
-        ))}
+
+                <div
+                  className="card-divider"
+                  style={{ marginTop: '0px', marginBottom: '0px' }}
+                />
+
+                <ClayCard.Body>
+                  <ClayCard.Row>
+                    <div className="autofit-col autofit-col-expand">
+                      <section className="autofit-section">
+                        <ClayCard.Description
+                          data-testid="stargazersCount"
+                          displayType="title"
+                        >
+                          Stars{' '}
+                          <span style={{ fontWeight: 400 }}>
+                            {stargazers_count}
+                          </span>
+                        </ClayCard.Description>
+
+                        <ClayCard.Description
+                          data-testid="forksCount"
+                          displayType="title"
+                        >
+                          Forks{' '}
+                          <span style={{ fontWeight: 400 }}>{forks_count}</span>
+                        </ClayCard.Description>
+
+                        <ClayCard.Description
+                          data-testid="openIssues"
+                          displayType="title"
+                        >
+                          Open issues{' '}
+                          <span style={{ fontWeight: 400 }}>
+                            {open_issues_count}
+                          </span>
+                        </ClayCard.Description>
+
+                        <ClayCard.Description
+                          data-testid="age"
+                          displayType="title"
+                        >
+                          Age{' '}
+                          <span style={{ fontWeight: 400 }}>
+                            {formatDistanceToNow(new Date(created_at), {
+                              addSuffix: true,
+                            })}
+                          </span>
+                        </ClayCard.Description>
+
+                        <ClayCard.Description
+                          data-testid="lastCommitAt"
+                          displayType="title"
+                        >
+                          Last commit{' '}
+                          <span style={{ fontWeight: 400 }}>
+                            {lastCommitAt
+                              ? formatDistanceToNow(new Date(lastCommitAt), {
+                                  addSuffix: true,
+                                })
+                              : 'No commits yet'}
+                          </span>
+                        </ClayCard.Description>
+
+                        <ClayCard.Description
+                          data-testid="license"
+                          displayType="title"
+                        >
+                          License{' '}
+                          <span style={{ fontWeight: 400 }}>
+                            {license?.name || 'N/A'}
+                          </span>
+                        </ClayCard.Description>
+
+                        <ClayCard.Caption>
+                          <ClayLabel data-testid="language" displayType="danger">
+                            {language}
+                          </ClayLabel>
+                        </ClayCard.Caption>
+                      </section>
+                    </div>
+                  </ClayCard.Row>
+                </ClayCard.Body>
+              </ClayCard>
+            </div>
+          )
+        )}
       </ClayLayout.Row>
     </ClayLayout.ContainerFluid>
   );
