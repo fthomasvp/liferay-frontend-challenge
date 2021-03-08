c2d0bf5e20b8c10c2cb223e1ea8f03710c4e7b2e
Fellipe Thomas <fellipecab@gmail.com>
2021-03-08 15:45:59 -0300

    test(header): update Props used on test cases

--- src/modules/components/header/index.test.tsx
+++ src/modules/components/header/index.test.tsx
@@ -2,45 +2,17 @@ import React from 'react';
 import { render, cleanup, fireEvent, waitFor } from '@testing-library/react';
 
 import Header from './index';
-import { GitHubRepo } from 'utils/types';
 
 describe('<Header />', () => {
   afterEach(cleanup);
 
-  const repositories: GitHubRepo[] = [
-    {
-      id: 22377139,
-      full_name: 'liferay/clay',
-      created_at: '2014-07-29T11:07:36Z',
-      stargazers_count: 156,
-      language: 'HTML',
-      forks_count: 185,
-      open_issues_count: 55,
-      license: {
-        name: 'Other',
-      },
-      lastCommitAt: '2021-03-05T12:23:22Z',
-    },
-  ];
-  const addRepository = jest.fn();
-
   test('should render correctly', () => {
-    const props = {
-      repositories,
-      addRepository,
-    };
-
-    const { container } = render(<Header {...props} />);
+    const { container } = render(<Header />);
     expect(container).toMatchSnapshot();
   });
 
   test('should display a Popover, a text input and two buttons', async () => {
-    const props = {
-      repositories,
-      addRepository,
-    };
-
-    const { getByTestId } = render(<Header {...props} />);
+    const { getByTestId } = render(<Header />);
 
     fireEvent.click(getByTestId('addButtonToolbarManagement'));
 
