106a6b3bb86a7652a50dde866359c99d4f9c37cb
fthomasvp <fellipecab@gmail.com>
2021-03-08 01:24:29 -0300

    refactor: wrap the Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { ClayIconSpriteContext } from '@clayui/icon';
-import spritemap from '@clayui/css/lib/images/icons/icons.svg';
+import Spritemap from '@clayui/css/lib/images/icons/icons.svg';
 
-import Dashboard from 'modules/pages/dashboard';
+import Routes from 'routes';
 
 const App = (): JSX.Element => {
   return (
-    <ClayIconSpriteContext.Provider value={spritemap}>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-        </Switch>
-      </Router>
+    <ClayIconSpriteContext.Provider value={Spritemap}>
+      <Routes />
     </ClayIconSpriteContext.Provider>
   );
 };
